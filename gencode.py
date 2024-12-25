dffe3500baf19fab18421332cab65ae80106273c
John Leung <john@johnleung.com>
2024-12-24 22:31:52 -0800

    Fixed layout not working due to x,y scales in toml

--- gencode.py
+++ gencode.py
@@ -5,17 +5,17 @@ import time
 from sys import argv, exit
 import subprocess
 
-filepath = argv[1] if len(argv) > 1 and argv[0].startswith('gencode') else argv[0]
-filename = filepath[:-4] if filepath[-4:] == '.svg' else ''
+filepathExt = argv[1] if len(argv) > 1 and argv[0].startswith('gencode') else argv[0]
+filepath = filepathExt[:-4] if filepathExt[-4:] == '.svg' else ''
 
-if not filename:
+if not filepath:
     print('Usage: gencode.py <svg file> [options]  where options are')
     print(' layout options if prefixed with l. All units in mm. Eg.:')
     print(' l:m30,a4 or l:m20,220x240')
     print(' st220x300 (scale to x, y, no "," if only option)')
     exit(0)
 
-svgname = filename[:-3] if filename[-3:] == 'Opt' else filename
+svgpath = filepath[:-3] if filepath[-3:] == 'Opt' else filepath
 allStrs = argv[2:] if len(argv) > 2 and argv[0].startswith('gencode') else []
 loStr = ''
 if allStrs:
@@ -34,7 +34,7 @@ for s in opStrs:
     if s[0:2] == 'st':
         x, y = s[2:].split('x')
         options.extend(['scaleto', x+'mm', y+'mm'])
-        svgname += '_st' + x + 'x' + y
+        svgpath += '_st' + x + 'x' + y
     else:
         raise Exception("Option "+s+" not implemented yet.")
 
@@ -43,24 +43,25 @@ for i, s in enumerate(loStrs):
         if i < len(loStrs) - 1:
             raise Exception("Page size needs to be last layout option")
         lOptions.extend([s])
-        svgname += '_' + s
+        svgpath += '_' + s
     elif s[0] == 'm':
         lOptions.extend(['-m', s[1:]+'mm'])
-        svgname += '_m' + s[1:]
+        svgpath += '_m' + s[1:]
     elif 'x' in s and not s.startswith('x'):
         lOptions.extend([s+'mm'])
-        svgname += '_' + s
+        svgpath += '_' + s
     else:
         raise Exception("Layout option "+s+" not implemented yet.")
 
 args = 'vpype -c cfg.toml'.split(' ') 
-rargs = ['read', filepath]
+rargs = ['read', filepathExt]
 wargs = 'gwrite -p plotter'.split(' ')
-layOpts = ['layout'] + lOptions
-outpath = svgname+'.gcode'
+mar = ['-m', '0'] if '-m' not in lOptions else [] # small & swing to a corner w/o margin
+layOpts = ['layout'] + mar + lOptions
+outpath = svgpath+'.gcode'
 
 start_time = time.time()
-print('cmd: ', ' '.join(args + rargs + options + layOpts + wargs + [outpath]))
+print('Running: ', ' '.join(args + rargs + options + layOpts + wargs + [outpath]))
 subprocess.run(args + rargs + options + layOpts + wargs + [outpath], check=True)
 duration = time.time() - start_time
 print(f"({duration:.2f} seconds)")
