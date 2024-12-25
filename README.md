23079d4b479b056e1cbd7e90a76be3190b354598
John Leung <john@johnleung.com>
2024-12-24 16:19:11 -0800

    Added options to gencode

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 Scripts to convert svg files to gcode.
 
-# Running
+## Running
 
 ```sh
 optimize.py SVGFILE [optimized-svg-file]
-gencode.py SVGFILE [gcode-file]
+gencode.py SVGFILE [options]
 ```
 
 The scripts assumes python3 is in brew path. If not, prepend `python3 `.
 
-# Dependencies
+## Dependencies
 
 * python3, and [vpype-gcode](https://github.com/plottertools/vpype-gcode/):
 
@@ -20,7 +20,7 @@ pipx install vpype
 pipx inject vpype vpype-gcode
 ```
 
-# Notes
+## Notes
 
 To set the direction of Y in the plotting device, issue the following commands:
 ```sh
@@ -31,3 +31,15 @@ $23=7
 $3=7 inverts Y. $23=7 then inverts Y homing direction so that it continues to home in the correct direction.
 
 Do the same in `cfg.toml` (`invert_y = true`), No `vertical_flip = true`.
+
+## vpype, SVG Notes
+
+Use `linesimplify` before `splitall` [issue 58](https://github.com/abey79/vpype/issues/58).
+
+Layers in SVG via `inkscape:groupmode="layer" inkscape:label="layername"` [Layer names]( https://bylr.info/articles/2023/03/17/layer-names/).
+
+## vpype Notes - Old, maybe outdated
+
+Currently, vpype doesn't know of a bounding box per se (independently of the actual vector content). So for vpype, whenever the bounding box is considered (e.g. for scaleto, to resize correctly), it uses the smallest rectangle that fits whatever vector data is in the pipeline.
+
+Of course, SVG does implement a specific bounding box (with the viewPort, width, height stuff), so this information is actually lost in read. I've considered adding that information into vpype's data model, so that a read/write sequence would maintain the actual margins. [issue 41](https://github.com/abey79/vpype/issues/41)
--- gencode.py
+++ gencode.py
@@ -1,5 +1,6 @@
 #!/opt/homebrew/bin/python3
 
+from math import e
 import time
 from sys import argv, exit
 import subprocess
@@ -8,15 +9,58 @@ filepath = argv[1] if len(argv) > 1 and argv[0].startswith('gencode') else argv[
 filename = filepath[:-4] if filepath[-4:] == '.svg' else ''
 
 if not filename:
-    print('Usage: gencode.py <svg file> [gcode file]')
+    print('Usage: gencode.py <svg file> [options]  where options are')
+    print(' layout options if prefixed with l. All units in mm. Eg.:')
+    print(' l:m30,a4 or l:m20,220x240')
+    print(' st220x300 (scale to x, y, no "," if only option)')
     exit(0)
 
-outpath = argv[2] if len(argv) > 2 and argv[0].startswith('gencode') else filename+'.gcode'
+svgname = filename[:-3] if filename[-3:] == 'Opt' else filename
+allStrs = argv[2:] if len(argv) > 2 and argv[0].startswith('gencode') else []
+loStr = ''
+if allStrs:
+    if allStrs[0].startswith('l:'):
+        loStr = allStrs.pop(0)
+    if len(allStrs) > 1 and allStrs[1].startswith('l:'):
+        loStr = allStrs.pop(1)
+opStr = allStrs[0] if allStrs else ''
+opStrs = opStr.split(',') if opStr else []
+loStrs = loStr[2:].split(',') if loStr else []
+# print('allStrs', allStrs, 'loStr', loStr, 'opStr', opStr, 'opStrs', opStrs)
 
-args = 'vpype --config cfg.toml read'.split(' ') 
+options = []
+lOptions = []
+for s in opStrs:
+    if s[0:2] == 'st':
+        x, y = s[2:].split('x')
+        options.extend(['scaleto', x+'mm', y+'mm'])
+        svgname += '_st' + x + 'x' + y
+    else:
+        raise Exception("Option "+s+" not implemented yet.")
+
+for i, s in enumerate(loStrs):
+    if s in "tight a6 a5 a4 a3 a2 a1 a0 letter legal executive tabloid".split(' '):
+        if i < len(loStrs) - 1:
+            raise Exception("Page size needs to be last layout option")
+        lOptions.extend([s])
+        svgname += '_' + s
+    elif s[0] == 'm':
+        lOptions.extend(['-m', s[1:]+'mm'])
+        svgname += '_m' + s[1:]
+    elif 'x' in s and not s.startswith('x'):
+        lOptions.extend([s+'mm'])
+        svgname += '_' + s
+    else:
+        raise Exception("Layout option "+s+" not implemented yet.")
+
+args = 'vpype -c cfg.toml'.split(' ') 
+rargs = ['read', filepath]
 wargs = 'gwrite -p plotter'.split(' ')
+layOpts = ['layout'] + lOptions
+outpath = svgname+'.gcode'
 
 start_time = time.time()
-subprocess.run(args + [filepath] + wargs + [outpath], check=True)
+print('cmd: ', ' '.join(args + rargs + options + layOpts + wargs + [outpath]))
+subprocess.run(args + rargs + options + layOpts + wargs + [outpath], check=True)
 duration = time.time() - start_time
 print(f"({duration:.2f} seconds)")
